e467354de46e4e72888afd9dcc1f1b7483991fc1
abolo <abolfazle0915501@gmail.com>
2025-06-10 21:27:15 +0200

    dfb

--- index.php
+++ index.php
@@ -3,57 +3,73 @@ include_once("library.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>chat</h1>
+    <h1>World wide chat</h1>
     <div>
         <div>
             <?php
-/* inserting into message table */
+            if (isset($_POST['logout'])) {
+                session_unset();
+                session_destroy();
+            }
 
-if(isset($_POST['submitTxt'])){
-    $inputMessage = $_POST['message'];
+            /* LOGIN  */
+            if (isset($_POST['login'])) {
+                header("location: login.php");
+            }
 
-    /*$findUserID = $connection->prepare('select userID from users where username = ?');
-    $findUserID->bind_param('s' , $_SESSION['username']);
-    $findUserID->execute();
-    $result = $findUserID->get_result();
-    $userID = $result->fetch_assoc();
-*/
-    
-    $insertInputs = $connection->prepare('insert into message(txt, sentByUserID) values (?,(select userID from users where username = ?))');
-    $insertInputs->bind_param('ss', $inputMessage, $_SESSION['username']);
-    $insertInputs->execute();
-}
+            /* inserting into message table */
 
-            $bringMessages = $connection->prepare('select * from message');
-            $bringMessages->execute();
-            $result = $bringMessages->get_result();
-            while($row=$result->fetch_assoc()){
-                $senderID = $row['sentByUserID'];
-                $findUsername = $connection->prepare('select username from users where userID = ? ');
-                $findUsername->bind_param('s' , $senderID);
-                $findUsername->execute();
-                $result = $findUsername->get_result();
-                $senderName = $result->fetch_assoc();
-                
-                $message = $row['txt'];
-                ?>
-<?=$senderName?> : <?=$message?> 
-                <?php
+            if (isset($_POST['submitTxt'])) {
+                $inputMessage = $_POST['message'];
+
+                $insertInputs = $connection->prepare('insert into message(txt, sentByUserID) values (?,(select userID from users where username = ?))');
+                $insertInputs->bind_param('ss', $inputMessage, $_SESSION['username']);
+                $insertInputs->execute();
+            }
+            if ($_SESSION["userLogin"] == true) {
+
+                $bringMessages = $connection->prepare('select * from message order by messageID');
+                $bringMessages->execute();
+                $messageResult  = $bringMessages->get_result();
+                while ($row = $messageResult->fetch_assoc()) {
+                    $senderID = $row['sentByUserID'];
+                    $findUsername = $connection->prepare('select username from users where userID = ?');
+                    $findUsername->bind_param('s', $senderID);
+                    $findUsername->execute();
+                    $resultOfquery = $findUsername->get_result();
+                    $senderRow = $resultOfquery->fetch_assoc();
+                    $senderName = $senderRow['username'];
+
+                    $message = $row['txt'];
+            ?>
+                    <?= $senderName ?> : <?= $message ?> <br>
+            <?php
+                }
+            } else {
+                header("location: login.php");
             }
             ?>
         </div>
         <div>
+            <h2>Welcome <?= $_SESSION['username'] ?></h2>
             <form method="post">
-                <input type="text" name="message" placeholder = "text here">
-                <input type="submit" name="submitTxt">
+                <input type="text" name="message" placeholder="text here">
+                <input type="submit" name="submitTxt" value="submit">
+                <input type="submit" name="login" value="login">
             </form>
         </div>
+        <form method="post">
+            <input type="submit" name="logout" value="logout">
+        </form>
     </div>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -3,73 +3,90 @@ include_once("library.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
-<?php
-if(isset($_POST['submit'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    <?php
+    if (isset($_POST['logout'])) {
+        session_unset();
+        session_destroy();
+    }
 
-    $loginCheck = $connection->prepare('select * from users where username = ?');
-    $loginCheck->bind_param('s' , $username);
-    if($loginCheck->execute()){
-    $result = $loginCheck->get_result();
-    if($row = $result->fetch_assoc()){
-        $userID = $row['userID'];
-        $username = $row['username'];
-        $userPass = $row['password'];
+    if (isset($_POST['goToChat'])) {
+        header("location: index.php");
+    }
 
-        if($password == $userPass){
-            $_SESSION["userLogin"] = true;
-            $_SESSION["username"] = $username;
-            header("location: index.php");
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $loginCheck = $connection->prepare('select * from users where username = ?');
+        $loginCheck->bind_param('s', $username);
+        if ($loginCheck->execute()) {
+            $result = $loginCheck->get_result();
+            if ($row = $result->fetch_assoc()) {
+                $userID = $row['userID'];
+                $username = $row['username'];
+                $userPass = $row['password'];
+
+                if ($password == $userPass) {
+                    $_SESSION["userLogin"] = true;
+                    $_SESSION["username"] = $username;
+                    header("location: index.php");
+                }
+            }
+        } else {
+            echo "user not found";
         }
     }
-}else{
-    echo "user not found";
-}
-}
 
-if(isset($_POST['submitCreation'])){
-    $newUsername = $_POST['usernameCreation'];
-    $newPass = $_POST['passwordCreation'];
-    $newPassConfir = $_POST['passwordCreationConfirm'];
+    if (isset($_POST['submitCreation'])) {
+        $newUsername = $_POST['usernameCreation'];
+        $newPass = $_POST['passwordCreation'];
+        $newPassConfir = $_POST['passwordCreationConfirm'];
 
-    if($newPass == $newPassConfir){
-        $signup = $connection->prepare('insert into users(username,password ) values(?,?)');
-        $signup->bind_param('ss', $newUsername,$newPassConfir);
-       if($signup->execute()){
-        echo "user created successfully.";
-       } ;
-
+        if ($newPass == $newPassConfir) {
+            $signup = $connection->prepare('insert into users(username,password ) values(?,?)');
+            $signup->bind_param('ss', $newUsername, $newPassConfir);
+            if ($signup->execute()) {
+                echo "user created successfully.";
+            };
+        }
     }
-}
-?>
+    ?>
 
     <div>
+        <h2>Welcome <?= $_SESSION['username'] ?></h2>
+
         <div>
             <h1>sign in</h1>
             <form method="post">
-                <input type="text" name="username" placeholder = "username">
-                <input type="password" name="password" placeholder = "password">
-                <input type="submit" name = "submit">
+                <input type="text" name="username" placeholder="username">
+                <input type="password" name="password" placeholder="password">
+                <input type="submit" name="submit">
             </form>
         </div>
         <h2>OR</h2>
         <div>
             <h1>sign up</h1>
             <form method="post">
-            <input type="text" name="usernameCreation" placeholder = "username">
-                <input type="password" name="passwordCreation" placeholder = "password">
-                <input type="password" name="passwordCreationConfirm" placeholder = "password confirmation">
-                <input type="submit" name = "submitCreation">
+                <input type="text" name="usernameCreation" placeholder="username">
+                <input type="password" name="passwordCreation" placeholder="password">
+                <input type="password" name="passwordCreationConfirm" placeholder="password confirmation">
+                <input type="submit" name="submitCreation">
             </form>
         </div>
+        <form method="post">
+            <input type="submit" name="logout" value="logout">
+            <input type="submit" name="goToChat" value="Chat_Page">
+        </form>
     </div>
 </body>
+
 </html>
